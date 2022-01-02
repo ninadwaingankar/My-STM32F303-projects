d4859c154e8a176f85ae84c9ee31daf667d5cfe9
NINAD WAINGANKAR <ninadnw88@gmail.com>
2022-01-02 13:31:31 +0530

    main.c for blinking of LED on STM32F303 using HAL driver
    updated

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "stm32f3xx_hal.h"
 
 
-//for LED sets
+//for LED sets for blinking
 #define LEDS1 (GPIO_PIN_8 | GPIO_PIN_10 | GPIO_PIN_12 | GPIO_PIN_14)
 #define LEDS2 (GPIO_PIN_9 | GPIO_PIN_11 | GPIO_PIN_13 | GPIO_PIN_15)
 
